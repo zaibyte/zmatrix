93e7810db652dd8f68961ea61b59665540399c99
templexxx <lucas1x1x@gmail.com>
2021-09-29 16:04:31 +0800

    keeper: refine methods

--- keeper/keeper.go
+++ keeper/keeper.go
@@ -4,11 +4,13 @@ import "g.tesamc.com/IT/zmatrix/db"
 
 // Keeper manages all DB.
 type Keeper interface {
-	// Remove clean up all resource of this DB.
-	Remove(dbID uint32) error
+	// CreateDB creates new db.DB.
+	CreateDB(dbID uint32, diskPath string) (*db.DB, error)
+	// RemoveDB clean up all resource of this DB.
+	RemoveDB(dbID uint32) error
 	// GetDB gets DB from Keeper.
 	GetDB(dbID uint32) (*db.DB, error)
 
-	// PickDisk picks up a disk for new database.
+	// PickDisk picks up a disk for a new database.
 	PickDisk() (diskPath string, err error)
 }
