ff60b7d3638aa9b01da53c4057301ca120d2fe95
templexxx <lucas1x1x@gmail.com>
2021-10-20 17:38:18 +0800

    db/engine/neo: defer stat for set/setbatch

--- db/engine/neo/neo.go
+++ db/engine/neo/neo.go
@@ -29,7 +29,7 @@ type Database struct {
 	id uint32
 	// volatile data, count bytes in lvl0 roughly for triggering flushing job to lvl1.
 	// After flushing, should minus bytes flushed.
-	lvl0Used       uint64
+	lvl0Used       int
 	lvl0DirtyCount uint64
 }
 
@@ -71,6 +71,17 @@ func (d *Database) GetID() uint32 {
 }
 
 func (d *Database) Set(key, value []byte) error {
+	d.doTrans()
+
+	var err error
+	defer func() {
+		if err == nil {
+			d.lvl0Used += len(key)
+			d.lvl0Used += len(value)
+			d.lvl0DirtyCount++
+		}
+	}()
+
 	panic("implement me")
 }
 
@@ -79,7 +90,31 @@ func (d *Database) Get(key []byte) ([]byte, io.Closer, error) {
 }
 
 func (d *Database) SetBatch(keys, values [][]byte) error {
+	d.doTrans()
+
+	var err error
+
+	defer func() {
+		if err == nil {
+
+			for i := range keys {
+				key := keys[i]
+				value := values[i]
+				d.lvl0Used += len(key)
+				d.lvl0Used += len(value)
+				d.lvl0DirtyCount++
+			}
+		}
+	}()
 	panic("implement me")
+}
+
+// 1. check threshold
+// 2. if matched, check if there is job already
+// 3. if no, run tran in new goroutine
+// 4. if yes, do nothing
+func (d *Database) doTrans() {
+
 }
 
 func (d *Database) Seal() error {
