1abb74d7778eba2be207fda464927133227775e5
templexxx <lucas1x1x@gmail.com>
2021-11-02 11:02:00 +0800

    db/engine/neo/lv1: testing idxItems sort.Sort interface

--- db/engine/neo/lv1_test.go
+++ db/engine/neo/lv1_test.go
@@ -1,1 +1,43 @@
 package neo
+
+import (
+	"encoding/binary"
+	"sort"
+	"testing"
+	"time"
+
+	"g.tesamc.com/IT/zaipkg/xmath/xrand"
+
+	"github.com/openacid/slim/index"
+)
+
+func TestIdxItemsSort(t *testing.T) {
+
+	cnt := 1024
+	items := make([]index.OffsetIndexItem, cnt)
+
+	keyBuf := make([]byte, 8)
+
+	xrand.Seed(time.Now().UnixNano())
+
+	for i := range items {
+		binary.LittleEndian.PutUint64(keyBuf, xrand.Uint64())
+		items[i] = index.OffsetIndexItem{
+			Key:    string(keyBuf),
+			Offset: int64(i),
+		}
+	}
+
+	sort.Sort(idxItems(items))
+
+	n := cnt
+	for i := n - 1; i > 0; i-- {
+		if items[i].Key < items[i-1].Key {
+			t.Fatal("not in asc order")
+		}
+	}
+}
+
+func TestMakeSegIdxHeader(t *testing.T) {
+
+}
