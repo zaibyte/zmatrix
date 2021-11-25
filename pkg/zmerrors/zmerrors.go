104515b3d3391ba2e3ea3bf7b4713e537657cc2b
templexxx <lucas1x1x@gmail.com>
2021-11-25 11:03:44 +0800

    pkg/zmerrors: add ErrDatabaseBroken

--- pkg/zmerrors/zmerrors.go
+++ pkg/zmerrors/zmerrors.go
@@ -11,4 +11,5 @@ var (
 	ErrTooManyDatabase = errors.New("too many database")
 	ErrTooFastSet      = xerrors.WithMessage(orpc.ErrTooManyRequests, "set too fast, pls wait for a while")
 	ErrDatabaseFull    = xerrors.WithMessage(orpc.ErrExtentFull, "database is full, pls create a new one")
+	ErrDatabaseBroken  = xerrors.WithMessage(orpc.ErrExtentBroken, "database is broken")
 )
